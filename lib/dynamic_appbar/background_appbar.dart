1efdb21129ff91857818d38875a27946c71c8ef8
Matatias <matatias97@gmail.com>
2023-05-17 12:46:00 +0700

    extract dynamic appbar backgroun

---
+++ lib/dynamic_appbar/background_appbar.dart
@@ -0,0 +1,84 @@
+import 'package:ck_linecode/dynamic_appbar/palette.dart';
+import 'package:ck_linecode/dynamic_appbar/widgets/icon_txt_row.dart';
+import 'package:flutter/material.dart';
+
+import 'constanta.dart';
+
+class MyBackgroundAppbar extends StatelessWidget {
+  const MyBackgroundAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: kToolbarHeight - 16),
+      padding: const EdgeInsets.only(left: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: CircleAvatar(
+                  backgroundColor: Palette.sunset,
+                  radius: 30,
+                  backgroundImage:
+                      Image.asset('assets/pmatatias.png', fit: BoxFit.cover)
+                          .image),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Container(
+                // color: Colors.blue,
+                margin: const EdgeInsets.only(top: 16),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Matatias Situmorang (pmatatias)",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      // Divider(),
+                      SizedBox(height: 8),
+                      Text(
+                        "Flutter developer | Blogger | Open-source \nenthusiast",
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ]),
+              ),
+            )
+          ]),
+          const SizedBox(height: 6),
+          const Divider(color: Colors.white, height: 16),
+          const Text("Lorem:", style: kTxtSubtitleSM),
+          const SizedBox(height: 4),
+          const Text(
+            klongLorem,
+            style: kTxtWhiteSemi,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(height: 6),
+          const Divider(color: Colors.white, height: 16),
+          const Text("Ipsum:", style: kTxtSubtitleSM),
+          const SizedBox(height: 4),
+          const IconTxtRow(
+              iconData: Icons.location_city, name: "Lorem Ipsum Dolor, PT."),
+          const IconTxtRow(iconData: Icons.phone, name: "0xx-xxx"),
+          const IconTxtRow(iconData: Icons.mail, name: "loremipsum@mail.com"),
+          // const SizedBox(height: 8),
+          const Divider(color: Colors.white, height: 16),
+          const Text("Rank:", style: kTxtSubtitleSM),
+          const Padding(
+              padding: EdgeInsets.symmetric(vertical: 3.0),
+              child: Text("101", style: kTxtWhiteSemi)),
+          const SizedBox(height: 4)
+        ],
+      ),
+    );
+  }
+}
--- lib/dynamic_appbar/dynamic_appbar.dart
+++ lib/dynamic_appbar/dynamic_appbar.dart
@@ -1,8 +1,8 @@
-import 'package:ck_linecode/dynamic_appbar/constanta.dart';
 import 'package:ck_linecode/dynamic_appbar/palette.dart';
-import 'package:ck_linecode/dynamic_appbar/widgets/icon_txt_row.dart';
 import 'package:ck_linecode/dynamic_appbar/widgets/list_content.dart';
 import 'package:flutter/material.dart';
+
+import 'background_appbar.dart';
 
 class DynamicAppbar extends StatefulWidget {
   const DynamicAppbar({super.key});
@@ -61,82 +61,7 @@ class _DynamicAppbarState extends State<DynamicAppbar> {
                     ? const Text('pmatatias Statistic',
                         style: TextStyle(fontSize: 16))
                     : const SizedBox(),
-                background: Container(
-                  margin: const EdgeInsets.only(top: kToolbarHeight-16),
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 16),
-                              child: CircleAvatar(
-                                  backgroundColor: Palette.sunset,
-                                  radius: 30,
-                                  backgroundImage: Image.asset(
-                                          'assets/pmatatias.png',
-                                          fit: BoxFit.cover)
-                                      .image),
-                            ),
-                            const SizedBox(width: 20),
-                            Expanded(
-                              child: Container(
-                                // color: Colors.blue,
-                                margin: const EdgeInsets.only(top: 16),
-                                child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: const [
-                                      Text(
-                                        "Matatias Situmorang (pmatatias)",
-                                        style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white),
-                                      ),
-                                      // Divider(),
-                                      SizedBox(height: 8),
-                                      Text(
-                                        "Flutter developer | Blogger | Open-source \nenthusiast",
-                                        style: TextStyle(color: Colors.white),
-                                      )
-                                    ]),
-                              ),
-                            )
-                          ]),
-                      const SizedBox(height: 6),
-                      const Divider(color: Colors.white, height: 16),
-                      const Text("Lorem:", style: kTxtSubtitleSM),
-                      const SizedBox(height: 4),
-                      const Text(
-                        klongLorem,
-                        style: kTxtWhiteSemi,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 6),
-                      const Divider(color: Colors.white, height: 16),
-                      const Text("Ipsum:", style: kTxtSubtitleSM),
-                      const SizedBox(height: 4),
-                      const IconTxtRow(
-                          iconData: Icons.location_city,
-                          name: "Lorem Ipsum Dolor, PT."),
-                      const IconTxtRow(iconData: Icons.phone, name: "0xx-xxx"),
-                      const IconTxtRow(
-                          iconData: Icons.mail, name: "loremipsum@mail.com"),
-                      // const SizedBox(height: 8),
-                      const Divider(color: Colors.white, height: 16),
-                      const Text("Rank:", style: kTxtSubtitleSM),
-                      const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 3.0),
-                          child: Text("101", style: kTxtWhiteSemi)),
-                          const SizedBox(height: 4)
-                    ],
-                  ),
-                ),
+                background: const MyBackgroundAppbar()
               ),
             )
           ],
