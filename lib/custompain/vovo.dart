1af5cf4d40ccc3963548d6998af04493b2577c5d
Matatias <matatias97@gmail.com>
2023-06-15 01:47:02 +0700

    test: release version waveball

--- lib/custompain/vovo.dart
+++ lib/custompain/vovo.dart
@@ -3,13 +3,6 @@ import 'package:ck_linecode/custompain/wave_left.dart';
 import 'package:ck_linecode/custompain/wave_right.dart';
 import 'package:flutter/material.dart';
 
-class Customparintsc extends StatefulWidget {
-  const Customparintsc({super.key});
-
-  @override
-  State<Customparintsc> createState() => _CustomparintscState();
-}
-
 const double sz = 200;
 
 // wave : 555x161
@@ -19,63 +12,53 @@ const double sz = 200;
 //sabit : 151x278
 /// p= 278/300 = 0.92
 /// l = 151/300 = 0.5
+class Customparintsc extends StatelessWidget {
+  Customparintsc({super.key, this.ew = false, required this.controller})
+      : val = Tween<double>(
+                begin: ((-sz * 0.425) - 5), end: (-(-sz * 0.425) + 5))
+            .animate(CurvedAnimation(parent: controller, curve: Curves.linear));
+  final bool ew;
+  final Animation<double> val;
+  final AnimationController controller;
 
-class _CustomparintscState extends State<Customparintsc> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Stack(
-          children: [
-            // Positioned(
-            //   bottom: 0,
-            //   right: 0,
-            //   child: CustomPaint(
-            //     size: Size(sz * 1.85, (sz * 0.537).toDouble()),
-            //     painter: RPSCustomPainter(),
-            //   ),
-            // ),
-            // Positioned(
-            //   bottom: 0,
-            //   left: 0,
-            //   child: CustomPaint(
-            //     size: Size(sz * 1.85, (sz * 0.537).toDouble()),
-            //     painter: RPSCustomPainter2(),
-            //   ),
-            // ),
-            
-
-            Container(
-              width: sz,
-              height: sz,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: const Color(0xFF4BB1FF).withOpacity(0.25),
-                    width: 5,
-                  ),
-                  gradient: RadialGradient(
-                    center: const Alignment(0.0, 0.0),
-                    tileMode: TileMode.clamp,
-                    // radius: 0.5,
-                    colors: <Color>[
-                      const Color.fromARGB(255, 236, 248, 255),
-                      const Color(0xFF70d3FF).withOpacity(0.6),
-                      const Color(0xFF7CCDFF).withOpacity(0.6),
-                      // Color(0xFF7CCDFF),
-                      // Color(0xFF70d3FF),
-                      // Colors.blue
-                    ],
-                    stops: const <double>[0.0, 0.98, 1.0],
-                  )),
-            ),
-            CircleAvatar(
+  Widget _buildchilf(BuildContext context, Widget? child) {
+    return Center(
+      child: Stack(
+        children: [
+          Container(
+            width: sz,
+            height: sz,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: const Color(0xFF4BB1FF).withOpacity(0.25),
+                  width: 5,
+                ),
+                gradient: RadialGradient(
+                  center: const Alignment(0.0, 0.0),
+                  tileMode: TileMode.clamp,
+                  // radius: 0.5,
+                  colors: <Color>[
+                    const Color.fromARGB(255, 236, 248, 255),
+                    const Color(0xFF70d3FF).withOpacity(0.6),
+                    const Color(0xFF7CCDFF).withOpacity(0.6),
+                    // Color(0xFF7CCDFF),
+                    // Color(0xFF70d3FF),
+                    // Colors.blue
+                  ],
+                  stops: const <double>[0.0, 0.98, 1.0],
+                )),
+          ),
+          Positioned(
+            left: 5,
+            right: 5,
+            bottom: 5,
+            child: CircleAvatar(
               backgroundColor: Colors.transparent,
               radius: sz / 2,
               child: ClipOval(
                 child: Container(
-                  transform: Matrix4.translationValues(sz*0.425, 0, 0),
+                  transform: Matrix4.translationValues(-val.value, 0, 0),
                   alignment: Alignment.bottomLeft,
                   child: UnconstrainedBox(
                     child: CustomPaint(
@@ -86,12 +69,17 @@ class _CustomparintscState extends State<Customparintsc> {
                 ),
               ),
             ),
-            CircleAvatar(
+          ),
+          Positioned(
+            left: 5,
+            right: 5,
+            bottom: 5,
+            child: CircleAvatar(
               backgroundColor: Colors.transparent,
               radius: sz / 2,
               child: ClipOval(
                 child: Container(
-                  transform: Matrix4.translationValues(-sz*0.425, 0, 0),
+                  transform: Matrix4.translationValues(val.value, 0, 0),
                   alignment: Alignment.bottomLeft,
                   child: UnconstrainedBox(
                     child: CustomPaint(
@@ -102,19 +90,31 @@ class _CustomparintscState extends State<Customparintsc> {
                 ),
               ),
             ),
-            Positioned(
-              right: 0,
-              bottom: 0,
-              top: 0,
-              child: CustomPaint(
-                size: Size(
-                    (0.5 * sz), (1.8410596026490067 * (sz * 0.92)).toDouble()),
-                painter: RPSCustomPainter3(),
-              ),
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: CustomPaint(
+              size: Size(
+                  (0.5 * sz), (1.8410596026490067 * (sz * 0.92)).toDouble()),
+              painter: RPSCustomPainter3(),
             ),
-          ],
-        ),
+          ),
+          // Card(
+          //   color: val.value ? Colors.amber : Colors.red,
+          //   child: const Text("s"),
+          // )
+        ],
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: _buildchilf,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:ck_linecode/provider_shop/pstate.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'custompain/vovo.dart';
+import 'custompain/prev.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(primarySwatch: Colors.amber),
-        home: const Customparintsc(),
+        home: const ViewWabe(),
       ),
     );
   }
