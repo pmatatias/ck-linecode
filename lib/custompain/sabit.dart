80f13da7f1231ff8f00380a375dde6f6b66a17e0
Matatias <matatias97@gmail.com>
2023-06-14 22:59:52 +0700

    checkpoint

---
+++ lib/custompain/sabit.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/cupertino.dart';
+
+
+//Copy this CustomPainter code to the Bottom of the File
+class RPSCustomPainter3 extends CustomPainter {
+    @override
+    void paint(Canvas canvas, Size size) {
+            
+Path path_0 = Path();
+    path_0.moveTo(size.width*0.08296689,size.height);
+    path_0.cubicTo(size.width*0.05530464,size.height,size.width*0.02738411,size.height*0.9993201,0,size.height*0.9979820);
+    path_0.arcToPoint(Offset(size.width*0.5923709,size.height*0.8382482),radius: Radius.elliptical(size.width*0.9128874, size.height*0.4958489),rotation: 0 ,largeArc: false,clockwise: false);
+    path_0.arcToPoint(Offset(size.width*0.8340596,size.height*0.5000000),radius: Radius.elliptical(size.width*0.9198212, size.height*0.4996151),rotation: 0 ,largeArc: false,clockwise: false);
+    path_0.arcToPoint(Offset(size.width*0.5923377,size.height*0.1617518),radius: Radius.elliptical(size.width*0.9198212, size.height*0.4996151),rotation: 0 ,largeArc: false,clockwise: false);
+    path_0.arcToPoint(Offset(0,size.height*0.002017986),radius: Radius.elliptical(size.width*0.9128874, size.height*0.4958489),rotation: 0 ,largeArc: false,clockwise: false);
+    path_0.cubicTo(size.width*0.02740397,size.height*0.0006798561,size.width*0.05531788,0,size.width*0.08296689,0);
+    path_0.arcToPoint(Offset(size.width*0.1767285,size.height*0.002582734),radius: Radius.elliptical(size.width*0.9282053, size.height*0.5041691),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.2677815,size.height*0.01015827),radius: Radius.elliptical(size.width*0.9161656, size.height*0.4976295),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.cubicTo(size.width*0.2973245,size.height*0.01345324,size.width*0.3268940,size.height*0.01760072,size.width*0.3556623,size.height*0.02247842);
+    path_0.cubicTo(size.width*0.3841060,size.height*0.02730216,size.width*0.4124636,size.height*0.03296043,size.width*0.4399205,size.height*0.03929137);
+    path_0.arcToPoint(Offset(size.width*0.5956887,size.height*0.08539209),radius: Radius.elliptical(size.width*0.9160199, size.height*0.4975504),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.7314040,size.height*0.1464460),radius: Radius.elliptical(size.width*0.9243907, size.height*0.5020971),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.8433841,size.height*0.2204460),radius: Radius.elliptical(size.width*0.9263775, size.height*0.5031763),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.9279338,size.height*0.3053957),radius: Radius.elliptical(size.width*0.9216954, size.height*0.5006331),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.cubicTo(size.width*0.9395497,size.height*0.3203669,size.width*0.9499205,size.height*0.3358237,size.width*0.9587682,size.height*0.3513345);
+    path_0.cubicTo(size.width*0.9677152,size.height*0.3670216,size.width*0.9753245,size.height*0.3831439,size.width*0.9813642,size.height*0.3992518);
+    path_0.cubicTo(size.width*0.9874901,size.height*0.4155719,size.width*0.9921656,size.height*0.4322734,size.width*0.9952715,size.height*0.4488921);
+    path_0.arcToPoint(Offset(size.width*0.9952715,size.height*0.5511367),radius: Radius.elliptical(size.width*0.9378609, size.height*0.5094137),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.cubicTo(size.width*0.9921788,size.height*0.5677590,size.width*0.9875033,size.height*0.5844604,size.width*0.9813642,size.height*0.6007770);
+    path_0.cubicTo(size.width*0.9753179,size.height*0.6168849,size.width*0.9677152,size.height*0.6330072,size.width*0.9587682,size.height*0.6486942);
+    path_0.cubicTo(size.width*0.9499205,size.height*0.6642050,size.width*0.9395629,size.height*0.6796619,size.width*0.9279338,size.height*0.6946331);
+    path_0.arcToPoint(Offset(size.width*0.8433841,size.height*0.7795647),radius: Radius.elliptical(size.width*0.9216954, size.height*0.5006331),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.7314040,size.height*0.8535647),radius: Radius.elliptical(size.width*0.9263444, size.height*0.5031583),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.5956887,size.height*0.9146187),radius: Radius.elliptical(size.width*0.9243974, size.height*0.5021007),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.4399205,size.height*0.9607194),radius: Radius.elliptical(size.width*0.9160530, size.height*0.4975683),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.cubicTo(size.width*0.4124636,size.height*0.9670504,size.width*0.3841126,size.height*0.9727086,size.width*0.3556689,size.height*0.9775324);
+    path_0.cubicTo(size.width*0.3268940,size.height*0.9824101,size.width*0.2973245,size.height*0.9865576,size.width*0.2677881,size.height*0.9898525);
+    path_0.arcToPoint(Offset(size.width*0.1767351,size.height*0.9974281),radius: Radius.elliptical(size.width*0.9162053, size.height*0.4976511),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.arcToPoint(Offset(size.width*0.08296689,size.height),radius: Radius.elliptical(size.width*0.9282185, size.height*0.5041763),rotation: 0 ,largeArc: false,clockwise: true);
+    path_0.close();
+
+Paint paint0Fill = Paint()..style=PaintingStyle.fill;
+// paint0Fill.color = const Color.fromRGBO(229,43,43,0.12).withOpacity(1.0);
+paint0Fill.color = const Color.fromRGBO(255,255,255,0.12).withOpacity(.20);
+
+canvas.drawPath(path_0,paint0Fill);
+
+}
+
+@override
+bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+}
+}
--- lib/custompain/vovo.dart
+++ lib/custompain/vovo.dart
@@ -1,5 +1,7 @@
+import 'package:ck_linecode/custompain/sabit.dart';
+import 'package:ck_linecode/custompain/wave_left.dart';
+import 'package:ck_linecode/custompain/wave_right.dart';
 import 'package:flutter/material.dart';
-import 'package:maapp/waveball/wave_left.dart';
 
 class Customparintsc extends StatefulWidget {
   const Customparintsc({super.key});
@@ -8,44 +10,111 @@ class Customparintsc extends StatefulWidget {
   State<Customparintsc> createState() => _CustomparintscState();
 }
 
+const double sz = 200;
+
+// wave : 555x161
+/// p = 551/300 =1.837
+/// l = 161/300 = 0.537
+
+//sabit : 151x278
+/// p= 278/300 = 0.92
+/// l = 151/300 = 0.5
+
 class _CustomparintscState extends State<Customparintsc> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-          child: Column(
-        children: [
-          CustomPaint(
-            size: Size(200, (200 * 1).toDouble()),
-            painter: RPSCustomPainter(),
-          ),
-          Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.amber,
-                border: Border.all(
-                  color: const Color(0xFF4BB1FF).withOpacity(0.25),
-                  width: 5,
+        child: Stack(
+          children: [
+            // Positioned(
+            //   bottom: 0,
+            //   right: 0,
+            //   child: CustomPaint(
+            //     size: Size(sz * 1.85, (sz * 0.537).toDouble()),
+            //     painter: RPSCustomPainter(),
+            //   ),
+            // ),
+            // Positioned(
+            //   bottom: 0,
+            //   left: 0,
+            //   child: CustomPaint(
+            //     size: Size(sz * 1.85, (sz * 0.537).toDouble()),
+            //     painter: RPSCustomPainter2(),
+            //   ),
+            // ),
+            
+
+            Container(
+              width: sz,
+              height: sz,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: const Color(0xFF4BB1FF).withOpacity(0.25),
+                    width: 5,
+                  ),
+                  gradient: RadialGradient(
+                    center: const Alignment(0.0, 0.0),
+                    tileMode: TileMode.clamp,
+                    // radius: 0.5,
+                    colors: <Color>[
+                      const Color.fromARGB(255, 236, 248, 255),
+                      const Color(0xFF70d3FF).withOpacity(0.6),
+                      const Color(0xFF7CCDFF).withOpacity(0.6),
+                      // Color(0xFF7CCDFF),
+                      // Color(0xFF70d3FF),
+                      // Colors.blue
+                    ],
+                    stops: const <double>[0.0, 0.98, 1.0],
+                  )),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: sz / 2,
+              child: ClipOval(
+                child: Container(
+                  transform: Matrix4.translationValues(sz*0.425, 0, 0),
+                  alignment: Alignment.bottomLeft,
+                  child: UnconstrainedBox(
+                    child: CustomPaint(
+                      size: Size(sz * 1.85, (sz * 0.537).toDouble()),
+                      painter: RPSCustomPainter2(),
+                    ),
+                  ),
                 ),
-                gradient: const RadialGradient(
-                  center: Alignment(0.0, 0.0),
-                  tileMode: TileMode.clamp,
-                  // radius: 0.5,
-                  colors: <Color>[
-                    // const Color(0xFF70d3FF).withOpacity(0.6),
-                    // const Color(0xFF7CCDFF).withOpacity(0.6),
-                    Color(0xFF7CCDFF),
-                    // Color(0xFF70d3FF),
-                    Colors.blue
-                  ],
-                  stops: <double>[0.0, .59],
-                )),
-          )
-        ],
-      )),
+              ),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: sz / 2,
+              child: ClipOval(
+                child: Container(
+                  transform: Matrix4.translationValues(-sz*0.425, 0, 0),
+                  alignment: Alignment.bottomLeft,
+                  child: UnconstrainedBox(
+                    child: CustomPaint(
+                      size: Size(sz * 1.85, (sz * 0.537).toDouble()),
+                      painter: RPSCustomPainter(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              bottom: 0,
+              top: 0,
+              child: CustomPaint(
+                size: Size(
+                    (0.5 * sz), (1.8410596026490067 * (sz * 0.92)).toDouble()),
+                painter: RPSCustomPainter3(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
