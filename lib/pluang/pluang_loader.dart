9918de915362a52dd3abde9fb7c3a89bdffadcb6
Petra Matatias Situmorang <matatias97@gmail.com>
2023-08-24 05:51:37 +0700

    plunag

---
+++ lib/pluang/pluang_loader.dart
@@ -0,0 +1,325 @@
+import 'package:flutter/material.dart';
+
+import 'calculate_metric.dart';
+
+class PaintPluang extends CustomPainter {
+  final Animation<double> _animation;
+
+  PaintPluang(this._animation) : super(repaint: _animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(size.width * 0.2358242, size.height * 0.4035136);
+    path_0.cubicTo(
+        size.width * 0.2581682,
+        size.height * 0.3508508,
+        size.width * 0.2834915,
+        size.height * 0.2992373,
+        size.width * 0.3116967,
+        size.height * 0.2488733);
+    path_0.cubicTo(
+        size.width * 0.3116967,
+        size.height * 0.2488733,
+        size.width * 0.3277720,
+        size.height * 0.2454246,
+        size.width * 0.3965682,
+        size.height * 0.2385263);
+    path_0.cubicTo(
+        size.width * 0.4653645,
+        size.height * 0.2316280,
+        size.width * 0.5351185,
+        size.height * 0.2315343,
+        size.width * 0.5695261,
+        size.height * 0.2488733);
+    path_0.cubicTo(
+        size.width * 0.5894597,
+        size.height * 0.2557716,
+        size.width * 0.6196777,
+        size.height * 0.2799165,
+        size.width * 0.6293223,
+        size.height * 0.2977386);
+    path_0.cubicTo(
+        size.width * 0.6323081,
+        size.height * 0.3032470,
+        size.width * 0.6468578,
+        size.height * 0.3381610,
+        size.width * 0.6331801,
+        size.height * 0.3736186);
+    path_0.cubicTo(
+        size.width * 0.6154076,
+        size.height * 0.4196144,
+        size.width * 0.5639431,
+        size.height * 0.4727712,
+        size.width * 0.5592464,
+        size.height * 0.4770932);
+    path_0.cubicTo(
+        size.width * 0.5379479,
+        size.height * 0.4975424,
+        size.width * 0.5129573,
+        size.height * 0.5146441,
+        size.width * 0.4853128,
+        size.height * 0.5276822);
+    path_0.cubicTo(
+        size.width * 0.4508294,
+        size.height * 0.5441186,
+        size.width * 0.3985313,
+        size.height * 0.5696822,
+        size.width * 0.3425834,
+        size.height * 0.5762754);
+    path_0.cubicTo(
+        size.width * 0.2866355,
+        size.height * 0.5828686,
+        size.width * 0.2689573,
+        size.height * 0.5878305,
+        size.width * 0.2171943,
+        size.height * 0.5886186);
+    path_0.cubicTo(
+        size.width * 0.1654313,
+        size.height * 0.5894068,
+        size.width * 0.1712227,
+        size.height * 0.5861102,
+        size.width * 0.1355355,
+        size.height * 0.5794237);
+    path_0.cubicTo(
+        size.width * 0.09984834,
+        size.height * 0.5727373,
+        size.width * 0.09181564,
+        size.height * 0.5673517,
+        size.width * 0.07381090,
+        size.height * 0.5518305);
+    path_0.cubicTo(
+        size.width * 0.05580616,
+        size.height * 0.5363093,
+        size.width * 0.03136498,
+        size.height * 0.5102119,
+        size.width * 0.03201900,
+        size.height * 0.4667500);
+    path_0.cubicTo(
+        size.width * 0.03240289,
+        size.height * 0.4412373,
+        size.width * 0.03537896,
+        size.height * 0.4064081,
+        size.width * 0.03780550,
+        size.height * 0.3977682);
+    path_0.cubicTo(
+        size.width * 0.04101711,
+        size.height * 0.3810805,
+        size.width * 0.04531090,
+        size.height * 0.3645742,
+        size.width * 0.05066398,
+        size.height * 0.3483318);
+    path_0.cubicTo(
+        size.width * 0.06883460,
+        size.height * 0.2985055,
+        size.width * 0.1207483,
+        size.height * 0.2218606,
+        size.width * 0.1207483,
+        size.height * 0.2218606);
+    path_0.cubicTo(
+        size.width * 0.1207483,
+        size.height * 0.2218606,
+        size.width * 0.1775924,
+        size.height * 0.1574186,
+        size.width * 0.2229810,
+        size.height * 0.1258593);
+    path_0.cubicTo(
+        size.width * 0.2515000,
+        size.height * 0.1062419,
+        size.width * 0.2821000,
+        size.height * 0.08916992,
+        size.width * 0.3143417,
+        size.height * 0.07488602);
+    path_0.cubicTo(
+        size.width * 0.3143417,
+        size.height * 0.07488602,
+        size.width * 0.4062275,
+        size.height * 0.04279322,
+        size.width * 0.4377346,
+        size.height * 0.03848407);
+    path_0.cubicTo(
+        size.width * 0.4692417,
+        size.height * 0.03417475,
+        size.width * 0.4767488,
+        size.height * 0.03058610,
+        size.width * 0.5181043,
+        size.height * 0.02928949);
+    path_0.cubicTo(
+        size.width * 0.5530616,
+        size.height * 0.02826377,
+        size.width * 0.5880521,
+        size.height * 0.02903581,
+        size.width * 0.6229100,
+        size.height * 0.03160263);
+    path_0.cubicTo(
+        size.width * 0.6634171,
+        size.height * 0.03633992,
+        size.width * 0.7076872,
+        size.height * 0.04736822,
+        size.width * 0.7225687,
+        size.height * 0.05055466);
+    path_0.cubicTo(
+        size.width * 0.7480616,
+        size.height * 0.05671356,
+        size.width * 0.7729953,
+        size.height * 0.06459322,
+        size.width * 0.7971564,
+        size.height * 0.07412669);
+    path_0.cubicTo(
+        size.width * 0.8337251,
+        size.height * 0.08846144,
+        size.width * 0.8752844,
+        size.height * 0.1230466,
+        size.width * 0.8865308,
+        size.height * 0.1344873);
+    path_0.cubicTo(
+        size.width * 0.9240142,
+        size.height * 0.1726229,
+        size.width * 0.9529668,
+        size.height * 0.2023742,
+        size.width * 0.9643318,
+        size.height * 0.2954504);
+    path_0.cubicTo(
+        size.width * 0.9756967,
+        size.height * 0.3885267,
+        size.width * 0.9347915,
+        size.height * 0.4609407,
+        size.width * 0.9206114,
+        size.height * 0.4868771);
+    path_0.cubicTo(
+        size.width * 0.9044692,
+        size.height * 0.5154958,
+        size.width * 0.8846161,
+        size.height * 0.5423136,
+        size.width * 0.8614597,
+        size.height * 0.5667797);
+    path_0.cubicTo(
+        size.width * 0.8357393,
+        size.height * 0.5949492,
+        size.width * 0.8035924,
+        size.height * 0.6311653,
+        size.width * 0.6994313,
+        size.height * 0.6921017);
+    path_0.lineTo(size.width * 0.6100569, size.height * 0.7352161);
+    path_0.lineTo(size.width * 0.5226114, size.height * 0.7628093);
+    path_0.lineTo(size.width * 0.4396725, size.height * 0.7829322);
+    path_0.lineTo(size.width * 0.4017578, size.height * 0.7881059);
+    path_0.cubicTo(
+        size.width * 0.4017578,
+        size.height * 0.7881059,
+        size.width * 0.3714972,
+        size.height * 0.8485805,
+        size.width * 0.3451749,
+        size.height * 0.8800847);
+    path_0.cubicTo(
+        size.width * 0.3103597,
+        size.height * 0.9217585,
+        size.width * 0.2708531,
+        size.height * 0.9531737,
+        size.width * 0.2558009,
+        size.height * 0.9594153);
+    path_0.cubicTo(
+        size.width * 0.2435882,
+        size.height * 0.9638729,
+        size.width * 0.2308777,
+        size.height * 0.9671483,
+        size.width * 0.2178863,
+        size.height * 0.9691864);
+    path_0.cubicTo(
+        size.width * 0.2084678,
+        size.height * 0.9697712,
+        size.width * 0.1990156,
+        size.height * 0.9697712,
+        size.width * 0.1895972,
+        size.height * 0.9691864);
+    path_0.cubicTo(
+        size.width * 0.1802863,
+        size.height * 0.9684322,
+        size.width * 0.1710469,
+        size.height * 0.9670847,
+        size.width * 0.1619479,
+        size.height * 0.9651610);
+    path_0.lineTo(size.width * 0.1375118, size.height * 0.9507881);
+    path_0.cubicTo(
+        size.width * 0.1258768,
+        size.height * 0.9349831,
+        size.width * 0.1198341,
+        size.height * 0.9288729,
+        size.width * 0.1150095,
+        size.height * 0.9134237);
+    path_0.cubicTo(
+        size.width * 0.1149573,
+        size.height * 0.9132585,
+        size.width * 0.1037872,
+        size.height * 0.8640042,
+        size.width * 0.1053654,
+        size.height * 0.8352415);
+    path_0.cubicTo(
+        size.width * 0.1075185,
+        size.height * 0.8012373,
+        size.width * 0.1124602,
+        size.height * 0.7674237,
+        size.width * 0.1201517,
+        size.height * 0.7340678);
+    path_0.lineTo(size.width * 0.1465118, size.height * 0.6288686);
+    path_0.cubicTo(
+        size.width * 0.1465118,
+        size.height * 0.6288686,
+        size.width * 0.1578867,
+        size.height * 0.5928517,
+        size.width * 0.1826782,
+        size.height * 0.5276949);
+    path_0.cubicTo(
+        size.width * 0.1957682,
+        size.height * 0.4931441,
+        size.width * 0.2209806,
+        size.height * 0.4378263,
+        size.width * 0.2358242,
+        size.height * 0.4035136);
+    path_0.close();
+
+    // final animationPercent = _animation.value;
+    final tailtem = _strokeTailTween.evaluate(_animation);
+    // final tailPercent = (tailtem +
+    //         _rotationTween.evaluate(_animation) +
+    //         _offsetTween.evaluate(_animation)) /
+    //     2.7;
+    // final headPercent = max((_strokeHeadTween.evaluate(_animation)), _epsilon);
+    const tailPercent = 5.3;
+    const headPercent = 1.7;
+
+    final path = drawAnimatedPath(path_0,
+        headpercent: _animation.value, tailpercent: _animation.value);
+
+    Paint paint0Fill = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = size.width * 0.06398104
+      ..strokeCap = StrokeCap.round;
+    paint0Fill.color = const Color(0xff000000).withOpacity(1.0);
+    canvas.drawPath(path, paint0Fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+const double _epsilon = .011;
+
+const int _kIndeterminateCircularDuration = 1333 * 2222;
+int kPath = _kIndeterminateCircularDuration ~/ 1333;
+int kRotae = _kIndeterminateCircularDuration ~/ 2222;
+
+final Animatable<double> _strokeHeadTween = CurveTween(
+  curve: const Interval(0.0, 0.5, curve: Curves.fastOutSlowIn),
+).chain(CurveTween(
+  curve: SawTooth(kPath),
+));
+final Animatable<double> _strokeTailTween = CurveTween(
+  curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+).chain(CurveTween(
+  curve: SawTooth(kPath),
+));
+final Animatable<double> _offsetTween = CurveTween(curve: SawTooth(kPath));
+final Animatable<double> _rotationTween = CurveTween(curve: SawTooth(kRotae));
