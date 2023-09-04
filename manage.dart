ac3f28bc838238bbb5a114e62ba6e2caa1361f9e
Matatias <matatias97@gmail.com>
2023-09-04 10:48:59 +0700

    nwnwnnwnwn

--- manage.dart
+++ manage.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:args/command_runner.dart';
 
 void main(List<String> arguments) {
@@ -9,10 +8,9 @@ void main(List<String> arguments) {
       'git diff --shortstat 4b825dc642cb6eb9a060e54bf8d69288fbee4904';
   // final args = ['status', ...arguments];
 
-  if(arguments[0] == 'xt'){
+  if (arguments[0] == 'xt') {
     print("masuk sini");
     Process.run(countLines, arguments);
-    
   }
   // Process.run(baseCommand, arguments).then((result) {
   //   print(result.stdout);
